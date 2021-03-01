a2188607da51401e0ae8be3735e51cddfda1ae4e
yanliang <943364131@qq.com>
2021-03-01 21:00:00 +0800

    Update remote_client.cpp

--- tests/kv/remote_client.cpp
+++ tests/kv/remote_client.cpp
@@ -91,19 +91,19 @@ int main(int argc, char **argv) {
     }
 
     std::string str2 = timestamps();
-    int time1=0, time2=0;
-    int j=0 , k=0;
-    for(int i=0; i<=11; i++){
-        if(str1.at(i)>='0' && str1.at(i)<='9'){
-            time1 += (str1.at(i)-'0')*pow(10,8-j);
-            j++;
-        }
-        if(str2.at(i)>='0' && str2.at(i)<='9'){
-            time2 += (str2.at(i)-'0')*pow(10,8-k);
-            k++;
-        }
-    }
-    int time3 = time2 - time1;
+
+    int h1=str1.find(':') , m1=str1.rfind(':'), s1=str1.find('.');
+    int h2=str2.find(':') , m2=str2.rfind(':'), s2=str2.find('.');
+    string hr1=str1.substr(0, h1), hr2=str2.substr(0, h2);
+    string min1=str1.substr(h1+1, m1-h1-1), min2=str2.substr(h2+1, m2-h2-1); 
+    string sec1=str1.substr(m1+1, s1-m1-1), sec2=str2.substr(m2+1, s2-m2-1);
+    string usec1=str1.substr(s1+1, 3), usec2=str2.substr(s2+1, 3); 
+    int hr3=atoi(hr2.c_str())-atoi(hr1.c_str());
+    int min3=atoi(min2.c_str())-atoi(min1.c_str());
+    int sec3=atoi(sec2.c_str())-atoi(sec1.c_str());
+    int usec3=atoi(usec2.c_str())-atoi(usec1.c_str());
+    int time3=hr3*60*60*1000+min3*60*1000+sec3*1000+usec3;
+    cout << time3;
 
     LogInfo("Results: " << completed_gets << " gets, " << completed_puts << " puts, Total consume " << time3 << "ms");
     std::this_thread::sleep_for(std::chrono::seconds(1));
